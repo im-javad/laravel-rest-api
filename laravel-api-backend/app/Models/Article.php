90516ff24a10a0eb5524dee289eeaf7c1abdbf2e
JavadJadidi <javadjadidi@gmail.com>
2023-05-23 16:03:22 +0330

    models-prepared

--- laravel-api-backend/app/Models/Article.php
+++ laravel-api-backend/app/Models/Article.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
+use App\Support\Traits\HasModel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory , HasModel;
+
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
