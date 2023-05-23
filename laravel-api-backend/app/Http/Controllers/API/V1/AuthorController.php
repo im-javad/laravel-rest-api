869807eaeb508988f5378258a91ffb43b176ed81
JavadJadidi <javadjadidi@gmail.com>
2023-05-23 16:45:44 +0330

    api-resource-was-implemented

--- laravel-api-backend/app/Http/Controllers/API/V1/AuthorController.php
+++ laravel-api-backend/app/Http/Controllers/API/V1/AuthorController.php
@@ -3,18 +3,16 @@
 namespace App\Http\Controllers\API\V1;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\V1\ArticleCollection;
-use App\Models\Article;
 use Illuminate\Http\Request;
 
-class ArticleController extends Controller
+class AuthorController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        return new ArticleCollection(Article::all());
+        //
     }
 
     /**
